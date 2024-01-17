e1d4183fb88dee46675baff457c7f8737d490b13
Ayyangurueswar <ayyan16.gbps@gmail.com>
2024-01-17 19:35:54 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Breadcrumbs from './components/Breadcrumbs';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App mx-20">
         <Header />
         <Breadcrumbs />
